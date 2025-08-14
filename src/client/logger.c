8e310fc9b962f989f717ff8f9b47b1a10c210566
AxelCovacich <axel.covacich@mi.unc.edu.ar>
2025-08-14 16:55:42 -0300

    chore: client logger implements mutex for thread-safe functioning

--- src/client/logger.c
+++ src/client/logger.c
@@ -1,14 +1,18 @@
 #include "logger.h"
+#include <pthread.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <time.h>
 
+static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Internal logger state, only used inside logger.c
 static FILE *pFile = NULL;
 
 int logger_init(const char *log_path) {
     // Initialization code for logger (e.g., open file at log_path)
 
-    mkdir(LOG_DIR, 0777);
+    mkdir(LOG_DIR, S_IRWXU | S_IRWXG | S_IRWXO); // Permits 0777
 
     pFile = fopen(log_path, "a");
     if (pFile == NULL) {
@@ -19,11 +23,12 @@ int logger_init(const char *log_path) {
 }
 
 void logger_log(const char *component, log_level level, const char *message) {
+    pthread_mutex_lock(&log_mutex); // Ensure thread safety
     if (pFile == NULL) {
-        fprintf(stderr, "Logger not initialized.\n");
+        fprintf(stderr, "Logger not initialized.\n"); // NOLINT
+        pthread_mutex_unlock(&log_mutex);
         return;
     }
-
     // Get current time
     time_t now = time(NULL);
     struct tm *utc_time = gmtime(&now);
@@ -31,8 +36,9 @@ void logger_log(const char *component, log_level level, const char *message) {
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", utc_time);
 
     // Write log entry
-    fprintf(pFile, "[%s] [%s] [%s]: %s\n", buffer, component, log_level_to_string(level), message);
-    fflush(pFile); // Ensure the log is written immediately
+    fprintf(pFile, "[%s] [%s] [%s]: %s\n", buffer, component, log_level_to_string(level), message); // NOLINT
+    fflush(pFile);                    // Ensure the log is written immediately
+    pthread_mutex_unlock(&log_mutex); // Release the mutex
 }
 
 void logger_close() {
