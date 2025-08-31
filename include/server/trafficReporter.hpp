ddb19f5eb8761bd439745aae00a8d6fb6935b2c3
AxelCovacich <axel.covacich@mi.unc.edu.ar>
2025-08-31 19:22:10 -0300

    feat: traffic module for metric gathering via prometheus-cpp

--- include/server/trafficReporter.hpp
+++ include/server/trafficReporter.hpp
@@ -7,36 +7,35 @@
 #ifndef TRAFFIC_REPORTER_HPP
 #define TRAFFIC_REPORTER_HPP
 
+#include "prometheus/exposer.h"
 #include <atomic>
 #include <optional>
+#include <prometheus/counter.h>
+#include <prometheus/registry.h>
 #include <string>
+#include <tuple>
 #include <vector>
 
+using LabelKey = std::tuple<std::string, std::string>; // protocol, direction
 class TrafficReporter {
   public:
-    TrafficReporter() = default;
+    TrafficReporter();
+    void initPrometheusMetrics(std::string metricHostPort);
+    void incrementMessage(std::string protocol, std::string direction);
+    void incrementError(std::string protocol, std::string direction);
+    void incrementReconnection(std::string protocol, std::string direction);
 
-    void onMessageReceived() {
-        m_stats.messageCount++;
-    }
-    void onError() {
-        m_stats.errorCount++;
-    }
-    void onReconnection() {
-        m_stats.reconnectionCount++;
-    }
-
-    std::string generateReport() const;
+    int getMessageCount(std::string protocol, std::string direction);
+    int getErrorCount(std::string protocol, std::string direction);
+    int getReconnectionCount(std::string protocol, std::string direction);
 
   private:
-    // Store traffic data
-    struct TrafficData {
-        std::atomic<int> messageCount{0};
-        std::atomic<int> errorCount{0};
-        std::atomic<int> reconnectionCount{0};
-    };
-
-    TrafficData m_stats;
+    prometheus::Family<prometheus::Counter> *m_api_msg_counter;
+    prometheus::Family<prometheus::Counter> *m_api_error_counter;
+    prometheus::Family<prometheus::Counter> *m_api_reconnection_counter;
+    std::map<LabelKey, prometheus::Counter *> m_message_counters;
+    std::map<LabelKey, prometheus::Counter *> m_error_counters;
+    std::map<LabelKey, prometheus::Counter *> m_reconnection_counters;
 };
 
 #endif // TRAFFIC_REPORTER_HPP
--- src/server/trafficReporter.cpp
+++ src/server/trafficReporter.cpp
@@ -1,12 +1,95 @@
 #include "trafficReporter.hpp"
 #include <sstream>
 
-std::string TrafficReporter::generateReport() const {
+using namespace prometheus;
 
-    std::ostringstream report;
-    report << "Traffic Report:\n";
-    report << "Messages Received: " << m_stats.messageCount.load() << "\n";
-    report << "Errors: " << m_stats.errorCount.load() << "\n";
-    report << "Reconnections: " << m_stats.reconnectionCount.load() << "\n";
-    return report.str();
+TrafficReporter::TrafficReporter()
+    : m_api_msg_counter(nullptr)
+    , m_api_error_counter(nullptr)
+    , m_api_reconnection_counter(nullptr)
+    , m_message_counters()
+    , m_error_counters()
+    , m_reconnection_counters() {
+}
+
+void TrafficReporter::initPrometheusMetrics(std::string metricHostPort) {
+
+    Exposer exposer(metricHostPort);
+
+    auto registry = std::make_shared<Registry>();
+
+    m_api_msg_counter =
+        &BuildCounter().Name("observed_messages_total").Help("Number of observed messages").Register(*registry);
+
+    m_api_error_counter =
+        &BuildCounter().Name("observed_errors_total").Help("Number of observed errors").Register(*registry);
+
+    m_api_reconnection_counter = &BuildCounter()
+                                      .Name("observed_reconnections_total")
+                                      .Help("Number of observed reconnections")
+                                      .Register(*registry);
+}
+
+void TrafficReporter::incrementMessage(std::string protocol, std::string direction) {
+
+    LabelKey key{protocol, direction};
+    auto tmp = m_message_counters.find(key);
+    if (tmp == m_message_counters.end()) {
+        auto &counter = m_api_msg_counter->Add({{"protocol", protocol}, {"direction", direction}});
+        m_message_counters[key] = &counter;
+        tmp = m_message_counters.find(key);
+    }
+    tmp->second->Increment();
+}
+
+void TrafficReporter::incrementError(std::string protocol, std::string direction) {
+
+    LabelKey key{protocol, direction};
+    auto tmp = m_error_counters.find(key);
+    if (tmp == m_error_counters.end()) {
+        auto &counter = m_api_error_counter->Add({{"protocol", protocol}, {"direction", direction}});
+        m_error_counters[key] = &counter;
+        tmp = m_error_counters.find(key);
+    }
+    tmp->second->Increment();
+}
+
+void TrafficReporter::incrementReconnection(std::string protocol, std::string direction) {
+
+    LabelKey key{protocol, direction};
+    auto tmp = m_reconnection_counters.find(key);
+    if (tmp == m_reconnection_counters.end()) {
+        auto &counter = m_api_reconnection_counter->Add({{"protocol", protocol}, {"direction", direction}});
+        m_reconnection_counters[key] = &counter;
+        tmp = m_reconnection_counters.find(key);
+    }
+    tmp->second->Increment();
+}
+
+int TrafficReporter::getErrorCount(std::string protocol, std::string direction) {
+    LabelKey key{protocol, direction};
+    auto tmp = m_error_counters.find(key);
+    if (tmp == m_error_counters.end()) {
+        return 0;
+    }
+    // static_cast to cast the value from double to int. There is no float usage in the system
+    return static_cast<int>(tmp->second->Value());
+}
+
+int TrafficReporter::getReconnectionCount(std::string protocol, std::string direction) {
+    LabelKey key{protocol, direction};
+    auto tmp = m_reconnection_counters.find(key);
+    if (tmp == m_reconnection_counters.end()) {
+        return 0;
+    }
+    return static_cast<int>(tmp->second->Value());
+}
+
+int TrafficReporter::getMessageCount(std::string protocol, std::string direction) {
+    LabelKey key{protocol, direction};
+    auto tmp = m_message_counters.find(key);
+    if (tmp == m_message_counters.end()) {
+        return 0;
+    }
+    return static_cast<int>(tmp->second->Value());
 }
