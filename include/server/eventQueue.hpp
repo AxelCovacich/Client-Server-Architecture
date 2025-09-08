80993a4d6b16df9d3db9f1248e92508ddd23581d
AxelCovacich <axel.covacich@mi.unc.edu.ar>
2025-09-08 20:57:22 -0300

    feat: event module now write logs

--- include/server/eventQueue.hpp
+++ include/server/eventQueue.hpp
@@ -1,6 +1,7 @@
 #ifndef EVENTQUEUE_HPP
 #define EVENTQUEUE_HPP
 
+#include "logger.hpp"
 #include <deque>
 #include <string>
 #include <unordered_map>
@@ -18,12 +19,14 @@ struct Event {
 
 class EventQueue {
   public:
-    EventQueue(int queueSize);
+    EventQueue(int queueSize, Logger &logger);
     void pushEvent(const std::string &clientID, const Event &event);
     bool popEvent(const std::string &clientID, Event &event);
 
   private:
     int m_queueSize;
     std::unordered_map<std::string, std::deque<Event>> m_eventQueueMap;
+
+    Logger &m_logger;
 };
 #endif // EVENTQUEUE_HPP
--- src/server/eventQueue.cpp
+++ src/server/eventQueue.cpp
@@ -1,9 +1,12 @@
 #include "eventQueue.hpp"
 #include <iostream>
 
-EventQueue::EventQueue(int queueSize)
+using namespace std;
+
+EventQueue::EventQueue(int queueSize, Logger &logger)
     : m_eventQueueMap()
-    , m_queueSize(queueSize) {
+    , m_queueSize(queueSize)
+    , m_logger(logger) {
     // Constructor implementation (if needed)
 }
 
@@ -11,12 +14,16 @@ void EventQueue::pushEvent(const std::string &clientID, const Event &event) {
     // no need to initialize the deque, operator[] does it if not exists
     if (m_eventQueueMap[clientID].size() >= m_queueSize) {
         m_eventQueueMap[clientID].pop_front(); // Remove oldest event if at capacity
+        m_logger.log(LogLevel::WARNING, "EventQueue",
+                     "Event queue for client " + clientID + " is full. Oldest event dropped.");
     }
     m_eventQueueMap[clientID].push_back(event);
+    m_logger.log(LogLevel::INFO, "EventQueue", "Event pushed to queue for client " + clientID);
 }
 
 bool EventQueue::popEvent(const std::string &clientID, Event &event) {
     auto tempIt = m_eventQueueMap.find(clientID);
+
     if (tempIt != m_eventQueueMap.end() && !tempIt->second.empty()) {
         event = tempIt->second.front();
         tempIt->second.pop_front();
@@ -24,7 +31,9 @@ bool EventQueue::popEvent(const std::string &clientID, Event &event) {
         if (tempIt->second.empty()) {
             m_eventQueueMap.erase(tempIt);
         }
+        m_logger.log(LogLevel::INFO, "EventQueue", "Event popped from queue for client " + clientID);
         return true;
     }
+    m_logger.log(LogLevel::INFO, "EventQueue", "No events to pop for client " + clientID);
     return false; // No events for this clientID
 }
